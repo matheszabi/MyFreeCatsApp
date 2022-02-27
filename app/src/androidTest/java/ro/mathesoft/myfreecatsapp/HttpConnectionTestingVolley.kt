98de7d3494515e923fba1e55c918a4d986f2b41f
matheszabi <matheszabi@gmail.com>
2022-02-27 12:22:01 +0200

    add a sample call and parse the json data

--- app/src/androidTest/java/ro/mathesoft/myfreecatsapp/HttpConnectionTestingVolley.kt
+++ app/src/androidTest/java/ro/mathesoft/myfreecatsapp/HttpConnectionTestingVolley.kt
@@ -1,6 +1,9 @@
 package ro.mathesoft.myfreecatsapp
 
+import android.util.JsonReader
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.json.JSONArray
+import org.json.JSONObject
 import org.junit.Assert
 import org.junit.Ignore
 import org.junit.Test
@@ -20,11 +23,39 @@ class HttpConnectionTestingVolley {
             Assert.assertEquals("Response code from $url not as expected", 200, responseCode)
         }
     }
+    @Ignore
     @Test
     fun testConnectionToThatApiGuy() {
         val url = URL("https://thatapiguy.com/")
         with(url.openConnection() as HttpURLConnection) {
             Assert.assertEquals("Response code from $url not as expected", 200, responseCode)
+        }
+    }
+
+    @Test
+    fun testRandomCatImageData() {
+        //Example
+        //Uses https://api.thecatapi.com/v1/images/search to show one random image every to “Another” is clicked.
+        val url = URL("https://api.thecatapi.com/v1/images/search")
+        with(url.openConnection() as HttpURLConnection) {
+            Assert.assertEquals("Response code from $url not as expected", 200, responseCode)
+            inputStream.bufferedReader().use {
+
+                val maybeJSON = it.readText()// [{"breeds":[],"id":"MjAwMjk5MQ","url":"https://cdn2.thecatapi.com/images/MjAwMjk5MQ.jpg","width":900,"height":600}]
+                val json = JSONArray(maybeJSON)
+                val firstJson = json.get(0) as JSONObject
+                val imgId = firstJson.getString("id")
+                val imgUrl = firstJson.getString("url")
+                val imgWidth = firstJson.getString("width")
+                val imgHeight = firstJson.getString("height")
+
+                Assert.assertNotNull(imgId)
+                Assert.assertNotNull(imgUrl)
+                Assert.assertNotNull(imgWidth)
+                Assert.assertNotNull(imgHeight)
+
+                //BUILD SUCCESSFUL in 59s
+            }
         }
     }
 }
